4344deb1b3fc29c020692c87a39704614eef97e4
jcmusic <johnmaloney@MacBook-Pro-2.local>
2019-06-24 14:04:23 -0700

    implemented delete

--- ang8-tourofheroes/src/app/hero.service.ts
+++ ang8-tourofheroes/src/app/hero.service.ts
@@ -27,6 +27,25 @@ export class HeroService {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    let id = typeof hero === 'number' ? hero : hero.id;
+    let url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions)
+    .pipe(
+      tap(_ => this.log(`deleted Hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
   /** PUT: update the hero on the server */
   updateHero (hero: Hero): Observable<any> {
     return this.http.put(this.heroesUrl, hero, this.httpOptions)
