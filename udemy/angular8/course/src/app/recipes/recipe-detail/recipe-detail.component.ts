21b88bb810d33fa3ccc0986417f0a8bacaa1b003
jcmusic <johnmaloney@MacBook-Pro-2.local>
2019-07-16 14:38:21 -0700

    update

--- udemy/angular8/course/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ udemy/angular8/course/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,12 +10,23 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-
   @Input() recipe: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(
+    private recipeService: RecipeService,
+    private shoppingListService: ShoppingListService
+  ) { }
 
   ngOnInit() {
+  }
+
+  toShoppingList(){
+    console.log('sending ingredients to the shoppingList');
+    this.recipe.ingredients.forEach(
+      (ingredient) => {
+        this.shoppingListService.addIngredient(ingredient);
+      }
+    );
   }
 
 }
--- udemy/angular8/course/src/app/shopping-list/shopping-list.service.ts
+++ udemy/angular8/course/src/app/shopping-list/shopping-list.service.ts
@@ -4,23 +4,26 @@ import { Ingredient } from '../shared/ingredient.model';
 
 // @Injectable(
 //     {providedIn: 'root', static: true; })
+@Injectable()
 export class ShoppingListService{
     ingredientsChanged = new EventEmitter<Ingredient[]>();
-    private ingredients : Ingredient[] = [
-        new Ingredient('Apples', 5),
-        new Ingredient('Tomatoes', 10)
-    ];
+    private ingredients : Ingredient[] = [];
+    // = [
+    //     new Ingredient('Apples', 5),
+    //     new Ingredient('Tomatoes', 10)
+    // ];
 
     getIngredients(){
         return this.ingredients.slice();
     }
     addIngredient(ingredient: Ingredient){
+        console.log('adding ingredient: ' + ingredient.name);
         this.ingredients.push(ingredient);
         this.broadcastChange();
     }
     removeIngredient(index: number){
         console.log('removing ingredient at index=' + index);
-        () => { 
+        (index:number) => { 
             // this.ingredients = this.ingredients.splice(index, 1);
             this.ingredients.splice(index, 1);
         }
